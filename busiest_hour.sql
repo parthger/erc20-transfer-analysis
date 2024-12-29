307378b0640eb7d2f21d64d2152ffba98969c2b3
parthger <106295881+parthger@users.noreply.github.com>
2024-12-29 20:01:37 +0530

    Update busiest_hour.sql

--- busiest_hour.sql
+++ busiest_hour.sql
@@ -1,10 +1,20 @@
+
+
 SELECT 
-    EXTRACT(hour FROM evt_block_time) AS transfer_hour,
-    COUNT(*) AS transfer_count
+   -- extracting hour component from the block timestamp
+   EXTRACT(hour FROM evt_block_time) AS transfer_hour,
+   
+   
+   COUNT(*) AS transfer_count
+
 FROM 
-    erc20_ethereum.evt_transfer
+   erc20_ethereum.evt_transfer  
+
 GROUP BY 
-    EXTRACT(hour FROM evt_block_time)
+   EXTRACT(hour FROM evt_block_time)  -- Grouping results by hour
+
 ORDER BY 
-    transfer_count DESC
-LIMIT 1;
+   transfer_count DESC  -- Sort by transfer count, highest first
+
+LIMIT 1;  -- Showing  hour with the highest activity.
+
